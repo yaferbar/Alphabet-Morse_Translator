ad36bfb2072427cb59d4a32281545d965a185d0e
Yafer Barajas <142061443+yaferbar@users.noreply.github.com>
2023-08-24 17:47:30 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# Text to Morse Code converter
+
+# Gets user entry
+quote = input('Write anything: ')
+
+# Dictionary with all the alphabetic letters and it morse equivalent
+morse_code_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....',
+    '7': '--...', '8': '---..', '9': '----.',
+    ' ': ' '
+}
+
+
+# Translate alphabetic to morse
+def translate(text):
+    morse_text = ''
+    for letter in text:
+        morse_text += f'{morse_code_dict[letter]} '
+    return f'Morse: {morse_text}'
+
+
+print(translate(quote.upper()))
